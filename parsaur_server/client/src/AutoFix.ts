3ca42bdfac42a44481c6335cc4d05e0d62063ce9
timen.stepisnik.perdih@in516ht.com <timen.stepisnik.perdih@in516ht.com>
2022-11-14 13:08:38 +0100

    AutoFix  documented

--- parsaur_server/client/src/AutoFix.ts
+++ parsaur_server/client/src/AutoFix.ts
@@ -1,5 +1,14 @@
 import * as vscode from 'vscode';
 
+ /**
+   * Calculates the Hamming distance between two strings.
+   * 
+   * @param a - First string
+   * @param b - Second string
+   * @param maxDistance - maximum allowed distance between the two strings
+   * 
+   * @returns - Boolean answer to whether the distance between the two strings is at most {@link maxDistance}
+   */
 const stringDistance = (a: string, b: string, maxDistance: number): boolean => {
 	const aL = a.length;
 	const bL = b.length;
@@ -27,12 +36,23 @@ const stringDistance = (a: string, b: string, maxDistance: number): boolean => {
 	}
 }
 
+ /**
+   * Provides VSCode Quick fixes for the Parsaur language. 
+   */
 export class AutoFix implements vscode.CodeActionProvider {
 
 	public static readonly providedCodeActionKinds = [
 		vscode.CodeActionKind.QuickFix
 	];
 
+ /**
+   * Extracts the word of the character. 
+   * 
+   * @param str - string in which the word we want to extract is found
+   * @param position - position of the character within the string
+   * 
+   * @returns The word of the character 
+   */
 	private getWordAt(str:string, position): [string, number, number] {
 		const isSpace = (c) => /\W/.exec(c);
 		let start = position - 1;
@@ -49,8 +69,17 @@ export class AutoFix implements vscode.CodeActionProvider {
 		end = Math.max(start, end);
 		
 		return [str.substring(start, end), start, end];
-	  }
+	}
 
+
+ /**
+   * Suggests code fix VSCode action. 
+   * 
+   * @param document - document content
+   * @param range - vscode.Range of editing position within the document
+   * 
+   * @returns Array of suggested fixes 
+   */
 	public provideCodeActions(document: vscode.TextDocument, range: vscode.Range): vscode.CodeAction[] | undefined {
 		let [fixes, start, stop] = this.extractFix(document, range);
 		if (fixes.length == 0) {
@@ -66,6 +95,14 @@ export class AutoFix implements vscode.CodeActionProvider {
 		return suggestedFixesArray;
 	}
 
+ /**
+   * Extracts possible fixes. 
+   * 
+   * @param document - document content
+   * @param range - vscode.Range of editing position within the document
+   * 
+   * @returns Array of the following elements: [string array of suggested code fixes, beginning position of the fix, ending position of the fix] .
+   */
 	private extractFix(document: vscode.TextDocument, range: vscode.Range): [Array<string>, number, number] {
 		const start = range.start;
 		const line = document.lineAt(start.line);
@@ -74,6 +111,13 @@ export class AutoFix implements vscode.CodeActionProvider {
 		return [autoFixes, startIx, stopIx];
 	}
 
+ /**
+   * Suggests code fixes. 
+   * 
+   * @param word - document word to be fixed
+   * 
+   * @returns Array of suggested fixes 
+   */
 	private suggestFix(word: string): string[] {
 		let arrayOfFixes = [];
 		for(var potentialFix of this.listOfAutoFixes){
@@ -89,6 +133,17 @@ export class AutoFix implements vscode.CodeActionProvider {
 		return arrayOfFixes;
 	}
 
+ /**
+   * Creates a code action quick fix. 
+   * 
+   * @param document - document content
+   * @param range - vscode.Range of editing position within the document
+   * @param stringFix - the suggested code fix string
+   * @param offsetLeft - position of the beginning of the fix 
+   * @param offsetRight - position of the ending of the fix
+   * 
+   * @returns Code action quick fix.
+   */
 	private createFix(document: vscode.TextDocument, range: vscode.Range, stringFix: string, offsetLeft: number, offsetRight: number): vscode.CodeAction {
 		const fix = new vscode.CodeAction(`Convert to ${stringFix}`, vscode.CodeActionKind.QuickFix);
 		fix.edit = new vscode.WorkspaceEdit();
@@ -96,7 +151,9 @@ export class AutoFix implements vscode.CodeActionProvider {
 		return fix;
 	}
 
-	
+ /**
+   * Array of possible code fixes.  
+   */
 	private listOfAutoFixes = [
 		"CONSTRUCTOR",
 		"CHARACTER",
