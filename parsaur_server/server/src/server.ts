5e319cd7de43091ffb300ae27a5a3a68394f8373
Timen Stepišnik Perdih <timen.stepisnik.perdih@in516ht.com>
2023-03-15 13:00:59 +0100

    version 0.1.7

--- parsaur_server/server/src/server.ts
+++ parsaur_server/server/src/server.ts
@@ -33,7 +33,6 @@ import {
 	TextDocument
 } from 'vscode-languageserver-textdocument';
 import { getOnDefinitionHandler } from './handlers/handleOnDefinition';
-import { WorkspaceFolders, WorkspaceFoldersFeature } from 'vscode-languageserver/lib/common/workspaceFolders';
 
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -41,10 +40,6 @@ const connection = createConnection(ProposedFeatures.all);
 
 // Create a simple text document manager.
 const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
-// const folders = WorkspaceFoldersFeature("");
-// const all = documents.all();
-// console.log("ALL");
-// console.log(folders);
 
 let hasConfigurationCapability = false;
 let hasWorkspaceFolderCapability = false;
