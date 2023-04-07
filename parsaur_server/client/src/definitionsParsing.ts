3031a9c0ac85fb8082b429de12b0cc7c70ff6327
Timen Stepišnik Perdih <timen.stepisnik.perdih@in516ht.com>
2023-04-07 11:18:23 +0200

    definitions parsing implemented

--- parsaur_server/client/src/definitionsParsing.ts
+++ parsaur_server/client/src/definitionsParsing.ts
@@ -15,6 +15,8 @@ const regularExpressions= [
 		name: "CREATE GRID "
 	}
 ];
+const IMPORT_STATEMENT = "IMPORT";
+const TAB_TO_SPACE_CONVERSION = 4;
 
 export function parseDefinitions(){
 	const definitionsDictionary = {};
@@ -31,47 +33,75 @@ export function parseDefinitions(){
 				if (doc){
 					const documentLines = doc?.split('\n');
 					for (let lineIx = 0; lineIx < documentLines.length; lineIx++){
-						for (const expression of regularExpressions){
-							const searchTerm = expression.name;
-							const termIx = documentLines[lineIx].indexOf(searchTerm); // This is also hierarchy depth if we assume that characters before the search term are \t
-							if (termIx > -1){
-								const extractTerm = documentLines[lineIx].substring(termIx + searchTerm.length, documentLines[lineIx].length-1);
-								const split = extractTerm.split(" ");
-								let extractedName = split[0];
-								if (extractedName.endsWith(";") || extractedName.endsWith("{"))
-									extractedName = extractedName.slice(0,-1);
-																
-								const contextLength = context.length; 
-								for (let i = 0; i < contextLength - termIx / 4; i++){ // update hierarchy depth based on number of used \t -> ONE \T IS 4 CHARACTERS
-									context.pop();
-									dictionaryKeyContext.pop();
-								}
-								let definitionKey = context.join(".") + "." + extractedName + "_" + uri;
-								if (context.length == 0)
-									definitionKey = definitionKey.substring(1, definitionKey.length -1);
-								definitionsDictionary[definitionKey] = {
-									name: extractedName,
-									context: context.join("."),
-									fileName: uri,
-									lineIndex: lineIx,
-									children: [],
-									imports: []
-								};
-								for (const dKey of dictionaryKeyContext){ // add children
-									definitionsDictionary[dKey]["children"].push(extractedName);
-								}
-								context.push(extractedName);
-								dictionaryKeyContext.push(definitionKey);
-								console.log(definitionsDictionary);
+						const importIx = documentLines[lineIx].indexOf(IMPORT_STATEMENT);
+						if (importIx > -1){
+							const contextLength = context.length; 
+							let whiteCharacters = TAB_TO_SPACE_CONVERSION; // 4 spaces equal to one \t
+							if (documentLines[lineIx].startsWith('\t'))
+								whiteCharacters = 1;
+							for (let i = 0; i < contextLength - importIx / whiteCharacters; i++){ // update hierarchy depth based on number of used \t -> ONE \T IS 4 CHARACTERS
+								context.pop();
+								dictionaryKeyContext.pop();
 							}
-						}
+							const spaceSplit = documentLines[lineIx].split(' ');
+							while (spaceSplit[spaceSplit.length - 1] === '')
+								spaceSplit.pop(); //remove unnecesarry spaces
+							let importTerm = spaceSplit[spaceSplit.length - 1];
+							while (importTerm.endsWith(';') || importTerm.endsWith('\r'))
+								importTerm = importTerm.slice(0,-1);
+							if (dictionaryKeyContext.length > 0)
+								definitionsDictionary[dictionaryKeyContext[dictionaryKeyContext.length - 1]]["imports"].push(importTerm);
+						} else {
+							for (const expression of regularExpressions){
+								const searchTerm = expression.name;
+								const termIx = documentLines[lineIx].indexOf(searchTerm); // This is also hierarchy depth if we assume that characters before the search term are \t
+								if (termIx > -1){
+									const extractTerm = documentLines[lineIx].substring(termIx + searchTerm.length, documentLines[lineIx].length-1);
+									const split = extractTerm.split(" ");
+									let extractedName = split[0];
+									while (extractedName.endsWith(';') || extractedName.endsWith('{') || extractedName.endsWith('\r'))
+										extractedName = extractedName.slice(0,-1);
+																	
+									const contextLength = context.length;
+									let whiteCharacters = TAB_TO_SPACE_CONVERSION; // 4 spaces equal to one \t
+									if (documentLines[lineIx].startsWith('\t'))
+										whiteCharacters = 1;
+									for (let i = 0; i < contextLength - termIx / whiteCharacters; i++){ // update hierarchy depth based on number of used \t -> ONE \T IS 4 CHARACTERS
+										context.pop();
+										dictionaryKeyContext.pop();
+									}
+									let definitionKey = context.join(".") + "." + extractedName + "_" + uri;
+									if (context.length == 0)
+										definitionKey = definitionKey.substring(1, definitionKey.length -1);
+									definitionsDictionary[definitionKey] = {
+										name: extractedName,
+										context: context.join("."),
+										fileName: uri,
+										lineIndex: lineIx,
+										children: [],
+										imports: []
+									};
+									// add children
+									if (dictionaryKeyContext.length > 0)
+										definitionsDictionary[dictionaryKeyContext[dictionaryKeyContext.length - 1]]["children"].push(extractedName);
+									
+									context.push(extractedName);
+									dictionaryKeyContext.push(definitionKey);
+									console.log(definitionsDictionary);
+								}
+							}
+						} // searching regular expressions 
 					}
 				}
 			});
-			return definitionsDictionary;
-
 		}); // open file
-	}); 
+	});
+	// console.log("Beginning post-parsing procedure");
+	// for (const keyName in definitionsDictionary){
+	// 	for (const importName of definitionsDictionary[keyName]['imports']){
+	// 		console.log();
+	// 	}
+	// }
 	console.log("Finished parsing definitions");
 	return definitionsDictionary;
 }
