b115797db18b78a0ed5c273bd97968685f6c1507
Timen Stepišnik Perdih <timen.stepisnik.perdih@in516ht.com>
2023-07-18 16:54:24 +0200

    parsing using brackets

--- parsaur_server/client/src/definitionsParsing.ts
+++ parsaur_server/client/src/definitionsParsing.ts
@@ -23,7 +23,6 @@ const IMPORT_STATEMENT = "IMPORT";
 const SINGLE_LINE_COMMENT_CHARACTER = "//";
 const MULTI_LINE_COMMENT_CHARACTER_OPEN = "/*";
 const MULTI_LINE_COMMENT_CHARACTER_CLOSE = "*/";
-const TAB_TO_SPACE_CONVERSION = 4;
 
  /**
    * Creates a dictionary of definitions. 
@@ -69,11 +68,14 @@ export async function parseDefinitions(){
 			const context = [];
 			const dictionaryKeyContext = [];
 			let ignoreLineDueToComment = false;
+			let openBracketStack = [] //true means context bracket, false means a constructor bracket
+			let contextBracket = false;
+			let lastExtractedName = "";
+			let lastDictionaryKey = "";
 			if (doc){
 				const documentLines = doc?.split('\n');
 				for (let lineIx = 0; lineIx < documentLines.length; lineIx++){
 					let currentLine = documentLines[lineIx];
-
 					// Check for comment characters
 					if (ignoreLineDueToComment){
 						const multi_line_comment_close_char_ix = currentLine.indexOf(MULTI_LINE_COMMENT_CHARACTER_CLOSE);
@@ -93,100 +95,110 @@ export async function parseDefinitions(){
 						currentLine = currentLine.substring(0, single_line_comment_char_ix);
 					}
 
-					const importIx = currentLine.indexOf(IMPORT_STATEMENT);
-					if (importIx > -1){
-						const contextLength = context.length; 
-						let whiteCharacters = TAB_TO_SPACE_CONVERSION; // 4 spaces equal to one \t
-						if (currentLine.startsWith('\t'))
-							whiteCharacters = 1;
-						for (let i = 0; i < contextLength - importIx / whiteCharacters; i++){ // update hierarchy depth based on number of used \t -> ONE \T IS 4 CHARACTERS
-							context.pop();
-							dictionaryKeyContext.pop();
-						}
-						const spaceSplit = currentLine.split(' ');
-						while (spaceSplit[spaceSplit.length - 1] === '')
-							spaceSplit.pop(); //remove unnecesarry spaces
-						let importTerm = spaceSplit[spaceSplit.length - 1];
-						while (importTerm.endsWith(';') || importTerm.endsWith('\r'))
-							importTerm = importTerm.slice(0,-1);
-						if (dictionaryKeyContext.length > 0){
-							const splitUri = uri.path.split('/');
-							splitUri.pop();
-							let replaced = importTerm.replace('\\', '/');
-							replaced = replaced.trim();
-							if (replaced.endsWith(';'))
-								replaced = replaced.slice(0,-1);
+					// check for '}' context closing
+					const splitByContextClosings = currentLine.split('}');
+					for (let i = 0; i<splitByContextClosings.length; i++){
+						// check for '{' context open
+						const splitByContextOpens = splitByContextClosings[i].split('{');
+						for (let j = 0; j<splitByContextOpens.length; j++){
+							currentLine = splitByContextOpens[j];
+							const importIx = currentLine.indexOf(IMPORT_STATEMENT);
+							if (importIx > -1){
+								const spaceSplit = currentLine.split(' ');
+								while (spaceSplit[spaceSplit.length - 1] === '')
+									spaceSplit.pop(); //remove unnecesarry spaces
+								let importTerm = spaceSplit[spaceSplit.length - 1];
+								while (importTerm.endsWith(';') || importTerm.endsWith('\r'))
+									importTerm = importTerm.slice(0,-1);
+								if (dictionaryKeyContext.length > 0){
+									const splitUri = uri.path.split('/');
+									splitUri.pop();
+									let replaced = importTerm.replace('\\', '/');
+									replaced = replaced.trim();
+									if (replaced.endsWith(';'))
+										replaced = replaced.slice(0,-1);
 
-							const fullPath = splitUri.join('/') + "/" + replaced;
-							definitionsDictionary[dictionaryKeyContext[dictionaryKeyContext.length - 1]].imports.push(fullPath);
-						}
-					} else {
-						// Special children using "?"
-						const trimmedLine = currentLine.trim();
-						let extractedName = "";
-						let termIx = -1;
-						let special = false;
-						if (trimmedLine.startsWith("?")){
-							special = true;
-							termIx = currentLine.indexOf("?");
-							if (trimmedLine.indexOf(" AS ") > -1){
-								const splittedByAs = trimmedLine.split(" AS "); // I assume there is only one "AS"
-								const asTerm = splittedByAs[splittedByAs.length - 1]; // split by { and ' '
-								extractedName = getSequenceAt(asTerm, 0);
-							}else{
-								const splittedByDot = trimmedLine.split(".");
-								extractedName = getSequenceAt(splittedByDot[splittedByDot.length - 1], 0);
-							}
-						}
-						if (!special){
-							// Regular expressions search for children
-							for (const expression of regularExpressions){
-								const searchTerm = expression.name;
-								termIx = currentLine.indexOf(searchTerm); // This is also hierarchy depth if we assume that characters before the search term are \t
+									const fullPath = splitUri.join('/') + "/" + replaced;
+									definitionsDictionary[dictionaryKeyContext[dictionaryKeyContext.length - 1]].imports.push(fullPath);
+								}
+							} else {
+								// Special children using "?"
+								const trimmedLine = currentLine.trim();
+								let extractedName = "";
+								let termIx = -1;
+								let special = false;
+								if (trimmedLine.startsWith("?")){
+									special = true;
+									termIx = currentLine.indexOf("?");
+									if (trimmedLine.indexOf(" AS ") > -1){
+										const splittedByAs = trimmedLine.split(" AS "); // I assume there is only one "AS"
+										const asTerm = splittedByAs[splittedByAs.length - 1]; // split by { and ' '
+										extractedName = getSequenceAt(asTerm, 0);
+									}else{
+										const splittedByDot = trimmedLine.split(".");
+										extractedName = getSequenceAt(splittedByDot[splittedByDot.length - 1], 0);
+									}
+								}
+								if (!special){
+									// Regular expressions search for children
+									for (const expression of regularExpressions){
+										const searchTerm = expression.name;
+										termIx = currentLine.indexOf(searchTerm); // This is also hierarchy depth if we assume that characters before the search term are \t
+										if (termIx > -1){
+											const extractTerm = currentLine.substring(termIx + searchTerm.length, currentLine.length);
+											const split = extractTerm.split(/\(| /);
+											extractedName = split[0];
+											while (extractedName.endsWith(';') || extractedName.endsWith('{') || extractedName.endsWith('\r') || extractedName.endsWith('\t') || extractedName.endsWith('\n'))
+												extractedName = extractedName.slice(0,-1);
+											break;
+										} 			
+									}	// searching regular expressions	
+								}
 								if (termIx > -1){
-									const extractTerm = currentLine.substring(termIx + searchTerm.length, currentLine.length-1);
-									const split = extractTerm.split(/\(| /);
-									extractedName = split[0];
-									while (extractedName.endsWith(';') || extractedName.endsWith('{') || extractedName.endsWith('\r') || extractedName.endsWith('\t') || extractedName.endsWith('\n'))
-										extractedName = extractedName.slice(0,-1);
-									break;
-								} 			
-							}	// searching regular expressions	
-						}
-						if (termIx > -1){
-							const contextLength = context.length;
-							let whiteCharacters = TAB_TO_SPACE_CONVERSION; // 4 spaces equal to one \t
-							if (currentLine.startsWith('\t'))
-								whiteCharacters = 1;
-							for (let i = 0; i < contextLength - termIx / whiteCharacters; i++){ // update hierarchy depth based on number of used \t -> ONE \T IS 4 CHARACTERS
+									const topLevel = context.length == 0;
+									const definitionKey = constructDictionaryKey(context, extractedName, uri);
+									const newEntry = new DefinitionEntry(
+										extractedName,
+										context.join("."),
+										topLevel,
+										uri.path,
+										[],
+										[],
+										[],
+										lineIx,
+										termIx
+									);
+									definitionsDictionary[definitionKey] = newEntry;
+									// add children
+									if (dictionaryKeyContext.length > 0){
+										definitionsDictionary[dictionaryKeyContext[dictionaryKeyContext.length - 1]].children.push(extractedName);
+										definitionsDictionary[dictionaryKeyContext[dictionaryKeyContext.length - 1]].childrenEntries.push(newEntry);
+									}
+									if (!special){
+										lastExtractedName = extractedName;
+										lastDictionaryKey = definitionKey;
+									}
+									contextBracket = true;
+								}		 
+							}
+							if (j != splitByContextOpens.length - 1){
+								openBracketStack.push(contextBracket);
+								if (contextBracket){
+									context.push(lastExtractedName);
+									dictionaryKeyContext.push(lastDictionaryKey);	
+								}
+								contextBracket = false;
+							}
+						} // context open
+						if (i != splitByContextClosings.length - 1){
+							const isContext = openBracketStack.pop();
+							if (isContext){
+								// Pop context
 								context.pop();
 								dictionaryKeyContext.pop();
 							}
-							const topLevel = context.length == 0;
-							const definitionKey = constructDictionaryKey(context, extractedName, uri);
-							const newEntry = new DefinitionEntry(
-								extractedName,
-								context.join("."),
-								topLevel,
-								uri.path,
-								[],
-								[],
-								[],
-								lineIx,
-								termIx
-							);
-							definitionsDictionary[definitionKey] = newEntry;
-							// add children
-							if (dictionaryKeyContext.length > 0){
-								definitionsDictionary[dictionaryKeyContext[dictionaryKeyContext.length - 1]].children.push(extractedName);
-								definitionsDictionary[dictionaryKeyContext[dictionaryKeyContext.length - 1]].childrenEntries.push(newEntry);
-							}
-							if (!special){
-								context.push(extractedName);
-								dictionaryKeyContext.push(definitionKey);	
-							}
-						}		 
-					} 
+						}
+					} // context closing 
 				}
 			}
 		});
